89ca7c9109686c702ac4e9c132858ea44a549331
CC-KEH <arbashhussain08@gmail.com>
2024-01-08 15:03:50 +0530

    Data Ingestion Component

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from src.logger import logging
+from src.exception import CustomException
+import os
+import sys
+
+
+@dataclass
+class DataIngestionConfig():
+    train_path:str = os.path.join('artifacts','train.csv')
+    test_path:str = os.path.join('artifacts','test.csv')
+    raw_path:str = os.path.join('artifacts','raw.csv')
+    
+class DataIngestion():
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+        
+    def initiate_ingestion(self):
+        logging.info('Ingestion Initiated')
+        
+        try:
+            df=pd.read_csv(os.path.join('notebooks/data/','gemstone.csv'))
+            logging.info('Dataset read as pandas Dataframe')
+            
+            os.makedirs(os.path.join(self.ingestion_config.raw_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_path,index=False)
+            
+            logging.info('Raw data is created')
+            
+            train_set,test_set = train_test_split(df,test_size=0.30,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_path,index=False,header=True)
+            
+            logging.info('Ingestion Completed')
+            
+            return(
+                self.ingestion_config.train_path,
+                self.ingestion_config.test_path
+            )
+                
+        except Exception as e:
+            logging.info('Exception occured at Data Ingestion Stage')
+            raise CustomException(e,sys)
